249ca003b499bc910aa9f371ed9713927a6f3d4f
Benjamin Guma <benjaminguma.bg@gmail.com>
2025-09-22 01:02:33 +0100

    refac(): updated config to auto redirect to dashboard

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
